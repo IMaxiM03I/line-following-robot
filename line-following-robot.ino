394a2ecd99beae846e9253e0c19e007b17e9090c
IMaxi_M03I <maxilea202@gmail.com>
2025-03-21 08:36:06 +0100

    extract speed manipulation to 'setSpeed', add additional constraint scenarios

--- line-following-robot.ino
+++ line-following-robot.ino
@@ -14,30 +14,76 @@
 #define IR_RIGHT_D 3    // 6
 
 // vehicle dynamics
-const int MAX_SPEED = 512;
+const int MAX_SPEED = 60;
+bool driving;
+
+void setSpeed(int l_speed, int r_speed) {
+  analogWrite(PWM_L, l_speed);
+  analogWrite(PWM_R, r_speed);
+}
 
 bool seeWhite(int sensor_pin) { return digitalRead(sensor_pin); }
 
 void setup() {
     pinMode(PWM_R, OUTPUT);
     pinMode(PWM_L, OUTPUT);
+    setSpeed(MAX_SPEED, MAX_SPEED);
+    driving = 0;
+
+    Serial.begin(9600);
 }
 
 void loop() {
-    // if right sensor detects white -> move right
-    if (seeWhite(IR_RIGHT_D) || seeWhite(IR_CRIGHT_D)) {
-        analogWrite(PWM_R, 0);
-        analogWrite(PWM_L, MAX_SPEED);
-    }
-    // if left sensor detects white -> move left
-    if (seeWhite(IR_LEFT_D) || seeWhite(IR_CRIGHT_D)) {
-        analogWrite(PWM_R, MAX_SPEED);
-        analogWrite(PWM_L, 0);
-    }
-    // if neither detects white -> move both
-    if (seeWhite(IR_CENTER_D)) {
-        analogWrite(PWM_R, MAX_SPEED);
-        analogWrite(PWM_L, MAX_SPEED);
-    }
-    // if both detect white -> we'll figure this out later
+
+  // we no longer see the straight/finish, turn on
+  if (!seeWhite(IR_ELEFT_D) && !driving) {
+    driving = 1;
+  }
+  
+  // we see the start/finish again, stop the car
+  if (seeWhite(IR_ELEFT_D) && driving) {
+    driving = 0;
+    setSpeed(0, 0);
+  }
+
+  // if right edge sees white, slow down
+  if (seeWhite(IR_ERIGHT_D)) {
+    // setSpeed(MAX_SPEED / 2, MAX_SPEED / 2);
+    Serial.println("edge right");
+  }
+
+  // skip all below if not driving
+  if (!driving) return;
+
+  // if we see only black, stop the car, ur getting robbed
+  if (!(seeWhite(IR_RIGHT_D) || seeWhite(IR_CRIGHT_D) || seeWhite(IR_CENTER_D) || seeWhite(IR_CLEFT_D) || seeWhite(IR_LEFT_D))) {
+    setSpeed(0, 0);
+    return;
+  }
+
+  // intersection
+  if (seeWhite(IR_RIGHT_D) && seeWhite(IR_LEFT_D)) {
+    setSpeed(MAX_SPEED, MAX_SPEED);
+    return;
+  }
+  
+  // if right sensor detects white -> move right
+  if (seeWhite(IR_RIGHT_D)) {
+    setSpeed(MAX_SPEED, 0);
+    Serial.println("right");
+    return;
+  }
+  // if left sensor detects white -> move left
+  if (seeWhite(IR_LEFT_D)) {
+    setSpeed(0, MAX_SPEED);
+    Serial.println("left");
+    return;
+  }
+
+  // if either center sensor detects white -> go straight
+  if (seeWhite(IR_CENTER_D) || seeWhite(IR_CRIGHT_D) || seeWhite(IR_CLEFT_D)) {
+    setSpeed(MAX_SPEED, MAX_SPEED);
+    Serial.println("straight");
+    return;
+  }
 }
